12ea2036375039c7453b48e07fcd96c88f351022
Toxa <untoxa@mail.ru>
2024-04-13 15:34:57 +0300

    reorganize assets: separate assets for the different targets

--- src/StateMenu.c
+++ src/StateMenu.c
@@ -2,17 +2,21 @@
 #include "main.h"
 
 #include <gbdk/platform.h>
+
+#include "SGB.h"
 #include "ZGBMain.h"
 #include "Scroll.h"
 #include "Keys.h"
 #include "Music.h"
 
 IMPORT_MAP(pressstarttilemap);
+IMPORT_BORDER(border);
 
 DECLARE_MUSIC(exo_start);
 
 void START(void) {
 	MAP_OVERLAP_SPR;
+	LOAD_SGB_BORDER(border);
 
 	InitScroll(BANK(pressstarttilemap), &pressstarttilemap, 0, 0);
 
