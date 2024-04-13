cd3ffe37d733b768e82084be582803603d8c3c44
Toxa <untoxa@mail.ru>
2024-04-13 13:19:57 +0300

    optimize tile replacement hook function

--- src/ZGBMain.c
+++ src/ZGBMain.c
@@ -5,21 +5,15 @@ UINT8 next_state = StateMenu;
 
 UINT8 GetTileReplacement(UINT8* tile_ptr, UINT8* tile) {
 	if(current_state == StateGame) {
-		*tile = 0;
 		switch(*tile_ptr) {
 			case 1:
 			case 2:
-				if(*(tile_ptr + 1) == 24) 
-					*tile = 23;
+				*tile = (*(tile_ptr + 1) == 24) ? 23 : 0;
 				return 255u;
 
 			default:
-				if(U_LESS_THAN(255 - (UINT16)*tile_ptr, N_SPRITE_TYPES)) {
-					return 255 - (UINT16)*tile_ptr;
-				}
+				return (*tile_ptr > (255u - N_SPRITE_TYPES)) ? ((*tile = 0), (255u - *tile_ptr)) : ((*tile = *tile_ptr), 255);
 		}
-		*tile = *tile_ptr;
 	}
-
 	return 255u;
 }
